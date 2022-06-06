0b0b0c8aa57cd6a1c72a3c36fc676ff27669ec79
Berkantyuks <byuksektepe@hotmail.com>
2022-06-06 03:16:48 +0300

    main.py updated

--- main.py
+++ main.py
@@ -4,13 +4,15 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
+def print_hi(msg):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hi, {msg}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi('There is a just main.py')
+    print("For run robot framework, use terminal")
+    print("Command: robot -d results/Amazon tests/Amazon.robot")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
